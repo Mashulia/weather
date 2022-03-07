cf51c03bfb53ace5be9aab426de909b5c08e423f
Mashulia <38750184+Mashulia@users.noreply.github.com>
2022-03-07 21:18:10 +0300

    Add files via upload

---
+++ js/tab.js
@@ -0,0 +1,29 @@
+
+const toggles = document.querySelectorAll(".screen__toggles-button");
+const tabContents = document.querySelectorAll(".screen__tab");
+
+
+export function toggleTab() {
+  for (var i = 0; i < toggles.length; i++) {
+
+    toggles[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      let activeTabButton = e.target.getAttribute("data-tab");
+
+      for (let j = 0; j < tabContents.length; j++) {
+        let activeContent = tabContents[j].getAttribute("data-tab-content");
+
+        if (activeTabButton === activeContent) {
+          tabContents[j].classList.add("screen__tab--active");
+          toggles[j].classList.add("screen__toggles-button--active");
+        } else {
+          tabContents[j].classList.remove("screen__tab--active");
+          toggles[j].classList.remove("screen__toggles-button--active");
+        }
+      };
+    });
+  }
+}
+
+// fetch(url)
+//   .then(data => console.log(data.json()))
