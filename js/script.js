447712e6237d6893ceed4d3795f8eb747e1752d3
Mashulia <38750184+Mashulia@users.noreply.github.com>
2022-03-07 23:18:38 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
-const toggles = document.querySelectorAll(".screen__toggles-button");
-const tabContents = document.querySelectorAll(".screen__tab");
+import { toggleTab } from './tab.js';
 
-
-for (var i = 0; i < toggles.length; i++) {
-
-  toggles[i].addEventListener("click", function (e) {
-    e.preventDefault();
-    let activeTabButton = e.target.getAttribute("data-tab");
-
-    for (let j = 0; j < tabContents.length; j++) {
-      let activeContent = tabContents[j].getAttribute("data-tab-content");
-
-      if (activeTabButton === activeContent) {
-        tabContents[j].classList.add("screen__tab--active");
-        toggles[j].classList.add("screen__toggles-button--active");
-      } else {
-        tabContents[j].classList.remove("screen__tab--active");
-        toggles[j].classList.remove("screen__toggles-button--active");
-      }
-    };
-  });
+const WEATHER_UI = {
+  SEARCH_FIELD: document.querySelector('.search__input'),
+  SEARCH: document.querySelector('.search'),
+  CITY_NAMES: document.querySelectorAll('.screen__city'),
+  CITY_TEMPERATURE: document.querySelector('.screen__temperature-number'),
+  WEATHER_ICON: document.querySelector('.screen__now-icon')
 }
+
+const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const ICON_URL = 'http://openweathermap.org/img/wn/'
+const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
+let cityName = 'Aktobe';
+let cityData;
+
+function getCityWeatherData() {
+  if (WEATHER_UI.SEARCH_FIELD.value) {
+    cityName = WEATHER_UI.SEARCH_FIELD.value;
+    WEATHER_UI.SEARCH_FIELD.value = "";
+    fetch(`${SERVER_URL}?q=${cityName}&appid=${API_KEY}&units=metric`)
+      .then((response) => {
+        return response.json();
+      })
+      .then(result => {
+        cityData = result;
+        showCityWeatherData()
+      })
+  }
+}
+
+function showCityWeatherData() {
+  WEATHER_UI.CITY_NAMES.forEach(element => {
+    element.textContent = cityName;
+  });
+  WEATHER_UI.CITY_TEMPERATURE.textContent = Math.round(cityData.main.temp);
+  WEATHER_UI.WEATHER_ICON.src = `${ICON_URL}${cityData.weather[0].icon}.png`;
+}
+
+WEATHER_UI.SEARCH.addEventListener("submit", getCityWeatherData)
+WEATHER_UI.SEARCH.addEventListener("submit", showCityWeatherData)
+
+
+toggleTab()
